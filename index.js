465a9817ce19a65653ae910cf7fe685b05210542
Moulesh2614 <mouleshc@gmail.com>
2023-05-04 20:32:50 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    if(req.url ==='/'){
+        
+        fs.readFile(path.join(__dirname,'index.html'), (err, content)=>{
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.end(content);
+        });
+    }
+    
+    else if(req.url ==='/api'){
+            fs.readFile(path.join(__dirname,'dormsCollection.json'),'utf-8',(err, content)=>{
+                if (err) throw err;
+                res.writeHead(200, {'Content-Type':'application/json'});
+                res.end(content);
+            })
+    }
+    else{
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.end(" <h1> 404 Nothing Found </h1>")
+    }
+})
+server.listen(3000, ()=> console.log("Our server is running"));
