1a2417a54f1976650355bbe5191f88f1c172cbc5
Moulesh2614 <mouleshc@gmail.com>
2023-05-05 00:47:37 -0400

    CORS

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
-
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions))
 const server = http.createServer((req,res)=>{
     console.log(req.url);
     if(req.url ==='/'){
